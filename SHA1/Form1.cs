b3a4ce6440f2af53c33678578e9fac45d84b44bd
ghila05 <nicolaghi05@gmail.com>
2022-03-23 13:55:06 +0100

    implementazione crypt-encript (da rivedere/studia)

--- SHA1/Form1.cs
+++ SHA1/Form1.cs
@@ -8,9 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Security.Cryptography; //libreria importata
+using System.IO;// libreria cript-encript
 
 namespace SHA1
 {
+
+   
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -31,8 +35,8 @@ namespace SHA1
         public string generaHash(string input)
         {
             SHA1CryptoServiceProvider sh = new SHA1CryptoServiceProvider();
-            sh.ComputeHash(Encoding.UTF8.GetBytes(input));
-            byte[] array = sh.Hash;
+            sh.ComputeHash(Encoding.UTF8.GetBytes(input)); //sfrutta il computer per creare l'hash 
+            byte[] array = sh.Hash; // crea array di byte 
             StringBuilder sb = new StringBuilder();
 
 
@@ -46,6 +50,132 @@ namespace SHA1
 
         }
 
-        
+
+
+
+        //------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            /*Aes aes = Aes.Create();
+            aes.GenerateIV(); // crea il vettore 
+            aes.GenerateKey(); // crea la chiave */
+
+            RijndaelManaged myRijndael = new RijndaelManaged();
+            myRijndael.GenerateKey();
+            myRijndael.GenerateIV();//se voglio mettere una mia chiave?
+
+            String original = "ciao";
+            // Encrypt the string to an array of bytes. 
+            byte[] encrypted = EncryptStringToBytes(original, myRijndael.Key, myRijndael.IV);
+
+            // Decrypt the bytes to a string. 
+            string roundtrip = DecryptStringFromBytes(encrypted, myRijndael.Key, myRijndael.IV);
+
+            //Display the original data and the decrypted data.
+            Console.WriteLine("Original:   {0}", original);
+            Console.WriteLine("Round Trip: {0}", roundtrip);
+
+
+
+            /*string s = Convert.ToString(aes);
+            textBox3.Text=(s);*/
+        }
+
+        public static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments. 
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+            // Create an RijndaelManaged object 
+            // with the specified key and IV. 
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for encryption. 
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            // Return the encrypted bytes from the memory stream. 
+            return encrypted;
+
+        }
+
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments. 
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold 
+            // the decrypted text. 
+            string plaintext = null;
+
+            // Create an RijndaelManaged object 
+            // with the specified key and IV. 
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for decryption. 
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream 
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+
     }
 }
